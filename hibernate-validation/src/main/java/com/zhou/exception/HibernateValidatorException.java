46a68c665f5ac441911d46b8188abde4486c833b
zhouy <412729287@qq.com>
2018-08-31 11:00:55 +0800

    后端校验框架hibernate-validation

--- hibernate-validation/src/main/java/com/zhou/exception/HibernateValidatorException.java
+++ hibernate-validation/src/main/java/com/zhou/exception/HibernateValidatorException.java
@@ -3,17 +3,43 @@ package com.zhou.exception;
 /**
  * Created by zhouyao on 2018/8/30.
  */
-public class HibernateValidatorException extends Exception{
+public class HibernateValidatorException extends RuntimeException{
 
+    private static final long serialVersionUID = -3655439959746149646L;
+    /**
+     * 错误编码
+     */
+    private String errorCode;
 
-
-
-
+    /**
+     * 错误消息
+     */
+    private String message;
 
     public HibernateValidatorException(String message) {
         super(message);
     }
 
+    public HibernateValidatorException(String errorCode,String message) {
+        super(message);
+        this.errorCode = errorCode;
+        this.message = message;
+    }
 
+    public String getErrorCode() {
+        return errorCode;
+    }
 
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
